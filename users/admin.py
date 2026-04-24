268c52dd4b2c41846b62967a96ff279f59f81c61
Bekbolsunn <karataevbekbolsun@gmail.com>
2026-04-24 23:16:51 +0600

    lesson1, CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.models import ConfirmationCode, CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ["id", "email", "is_active", "is_staff"]
+    list_editable = ["is_active"]
+    ordering = ["email"]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password", "is_active")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+
+@admin.register(ConfirmationCode)
+class ConfirmationCodeAdmin(admin.ModelAdmin):
+    list_display = ["id", "code"]
